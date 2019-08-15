3789cc37c44548be130db53bb1a613cf29d04504
erinpc <4540807@qq.com>
2019-08-15 15:00:43 +0800

    feat:implement baseapi class and basic test for single api

---
+++ hogwarts_apitest/api.py
@@ -0,0 +1,50 @@
+# !/user/bin/env python
+# coding:utf-8
+import requests
+
+
+class BaseAPI(object):
+    method ="GET"
+    url = ""
+    params = {}
+    headers = {}
+    data = {}
+    json = {}
+
+    def set_params(self,**params):
+        self.params = params
+        return self
+
+    def set_data(self,data):
+        self.data = data
+        return self
+
+    def set_json(self,json_data):
+        self.json = json_data
+        return self
+
+    def run(self):
+        self.response = requests.request(
+            self.method,
+            self.url,
+            params=self.params,
+            headers=self.headers,
+            # verify = False,
+            data =self.data,
+            json = self.json
+        )
+        return self
+
+    def validate(self, key, expected_value):
+        value = self.response
+        for _key in key.split("."):
+            if isinstance(value, requests.Response):
+                if _key == "json()":
+                    value = self.response.json()
+                else:
+                    value = getattr(value, _key)
+            elif isinstance(value, (requests.structures.CaseInsensitiveDict, dict)):
+                value = value[_key]
+
+        assert expected_value == value
+        return self
